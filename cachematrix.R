c41f72055d4ca59c943f2a683841ba7ea52de5f3
Vamshideep <vamshideep@gmail.com>
2014-04-27 13:47:10 -0700

    1st submission after creating functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
 ## The below functions create special matrix that can 
 ## calculate the matrix inversion for the first time and cache it for future use
 
-# The makeCachematrix function will create a special type of matrix that will be called from second function
+### The makeCachematrix function will create a special type of matrix that will be called from second function
+### It takes an argument with X of type matrix and it returns a list with 4 list items (they are actually 4 functions wrapped in a list)
 
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -19,10 +20,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-#This function calculates the inverse of the matrix
-
+### This function calculates the inverse of the matrix
+### It takes an argument with X of type makeCachematrix and it returns a list with 4 list items (they are actually 4 functions wrapped in a list)
+### The output is the mean coming whether from the special matrix above or from the computation
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ### Return a matrix that is the inverse of 'x'
   inv <- x$getInverse()
   if(!is.null(inv)) {
     message("getting cached data")
